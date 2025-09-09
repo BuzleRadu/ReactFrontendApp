685ab89655d47d50dee80bbab7e7c883e66082ea
BuzleRadu <157800093+BuzleRadu@users.noreply.github.com>
2025-09-09 13:46:22 +0300

    yeeey
    yeeey

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "b675495c51a3f5576c2c96b9e41a11c4";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
